a3d1af56733d5e743a06b7f07fc4a43560633812
LunaDevX <mouna1.saiss@gmail.com>
2025-09-17 11:17:02 +0100

    task3

--- ALL OF PROJECT/FonctionSAS/task3.c
+++ ALL OF PROJECT/FonctionSAS/task3.c
@@ -1,6 +1,6 @@
 #include<stdio.h>
 void typeannee (int A){
-    if (A%4==0 || A%100!=0 && A%400==0)
+    if (A%4==0 && A%100!=0 || A%400==0)
         printf ("--l'annee est bissextile--");
     else 
         printf("**L'annee pas bisextile**");
