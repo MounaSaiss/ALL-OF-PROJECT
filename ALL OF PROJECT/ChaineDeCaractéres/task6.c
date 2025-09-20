fbd4b66c80583d00a683cde06cd0b463e79a9317
LunaDevX <mouna1.saiss@gmail.com>
2025-09-20 13:59:06 +0100

    task6

--- ALL OF PROJECT/ChaineDeCaractéres/task6.c
+++ ALL OF PROJECT/ChaineDeCaractéres/task6.c
@@ -1,9 +1,15 @@
 #include<stdio.h>
 int main(){
 char C[100];
+int i;
+int nbr=0;
 printf ("Veuillez saisir une chaine de caractere :");
 scanf("%s",C);
-puts("la chaine de caractere est :");
-puts(C);
+i=0;
+do {
+  nbr++;
+  i++;
+}while(C[i]!='\0');
+printf("la longeur de chaine est : %d",nbr);
     return 0 ;
 }
